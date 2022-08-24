005bdaf75821a73f00e65d4fcc421e6611879b36
craig91 <craigdejean1@gmail.com>
2022-08-24 23:29:51 +0000

    Added cm::Ordering methods to code base

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io; 
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() { // entry point into the program
@@ -7,7 +8,7 @@ fn main() { // entry point into the program
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
-    
+
     println!("Please input your guess");
 
     let mut guess = String::new();
@@ -24,5 +25,13 @@ fn main() { // entry point into the program
         .read_line(&mut guess) // gets input from the user and stores the input in the guess variable
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
 }
