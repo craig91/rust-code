3c4b1a2c68a137705a174421920e3e99f9a0bd6e
craig91 <craigdejean1@gmail.com>
2022-08-24 23:06:23 +0000

    Added rand crate as a dependency, implemented num generator to code base

---
+++ guessing_game/target/debug/deps/cfg_if-ac68a5ec1f1c024f.d
@@ -0,0 +1,7 @@
+/home/cdejean/Code/rust-code/guessing_game/target/debug/deps/cfg_if-ac68a5ec1f1c024f.rmeta: /home/cdejean/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/home/cdejean/Code/rust-code/guessing_game/target/debug/deps/libcfg_if-ac68a5ec1f1c024f.rlib: /home/cdejean/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/home/cdejean/Code/rust-code/guessing_game/target/debug/deps/cfg_if-ac68a5ec1f1c024f.d: /home/cdejean/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+
+/home/cdejean/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-fa7a3f559d7f737f.d
@@ -0,0 +1,5 @@
+/home/cdejean/Code/rust-code/guessing_game/target/debug/deps/guessing_game-fa7a3f559d7f737f: src/main.rs
+
+/home/cdejean/Code/rust-code/guessing_game/target/debug/deps/guessing_game-fa7a3f559d7f737f.d: src/main.rs
+
+src/main.rs:
