ad1f09ac0bafb1c8b6922622fd8f013199837454
craig91 <craigdejean1@gmail.com>
2022-08-30 23:30:46 +0000

    Added more file i/o code

--- create-file/src/main.rs
+++ create-file/src/main.rs
@@ -1,4 +1,14 @@
-fn main() {
-    std::fs::File::create("empty.txt").expect("create failed");
-    println!("File created successfully");
+use std::fs;
+use std::io;
+
+fn main() -> std::io::Result<()> {
+    println!("Where do you want to create a directory?");
+    let mut dir = String::new();
+    io::stdin()
+        .read_line(&mut dir)
+        .expect("Failed to read input");
+        
+    fs::create_dir("{dir}")?;
+    println!("Directory created at {dir}");
+    Ok(())
 }
--- practice-app/src/main.rs
+++ practice-app/src/main.rs
@@ -1,11 +1,27 @@
-use std::io;
-use rand::Rng;
+//use std::io;
+// use rand::Rng;
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed: {guess}");
+    // println!("Guess the number!");
+    // let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {secret_number}");
+    // println!("Please input your guess.");
+    // let mut guess = String::new();
+    // io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // println!("You guessed: {guess}");
+
+
+    let tup = (500, 6.4, 1);
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is {y}");
+
+    let a: (i32, f64, u8) = (700, 60.4, 4);
+
+    let six_hundred = a.0;
+    let sixty_point_four = a.1;
+    
+    let four = a.2;
+
+    println!("{six_hundred}, {sixty_point_four}, {four}")
+
 }
